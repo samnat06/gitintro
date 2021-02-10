4f75ec543f2af9340788e6081a069736d00e4072
samnat06 <11318764+samnat06@users.noreply.github.com>
2021-02-10 11:38:24 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
+<<<<<<< HEAD
+// this is to create the logic for feature2"
+// feature 2 changes
+=======
 // this is to create the logic for feature1"
+>>>>>>> 979c456012eb6144fe01d17b0c16f04098f6a53a
